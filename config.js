5fe49752a72a55667b8e641d21557f28e63ba83e
nrg6477 <35335054+nrg6477@users.noreply.github.com>
2018-01-11 05:54:09 -0700

    Update config.js

--- config.js
+++ config.js
@@ -39,7 +39,7 @@ module.exports = {
 	update_interval: 60000, // In milliseconds
 
 	// List of accepted timezones: https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
-	default_timezone: "Europe/Madrid",
+	default_timezone: "US/Arizona",
 
 	// If this option is enabled, the bot will delete the message that triggered it, and its own
 	// response, after the specified amount of time has passed.
